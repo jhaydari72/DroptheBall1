95dccc40bcba1762632f8a944059c8ca96ccb9ef
jhaydari72@gmail.com <jhaydari72@gmail.com>
2020-10-21 13:13:55 -0700

    lvl 2 reworked

--- DroptheBall/Levels/Level1.gd
+++ DroptheBall/Levels/Level1.gd
@@ -7,5 +7,5 @@ func _ready():
 	pass 
 
 func _on_Timer_timeout():
-	value = get_tree().change_scene("res://Levels/Level1.tscn")
+	value = get_tree().change_scene("res://Levels/Level2.tscn")
 	return value
